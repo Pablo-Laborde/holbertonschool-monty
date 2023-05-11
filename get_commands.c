37cf08b77ce4d48ed63827060f7eca49298478b4
Pablo <p5.tacoronte@gmail.com>
2023-05-11 00:31:58 -0700

    new function to search for the correct function

--- get_commands.c
+++ get_commands.c
@@ -1,4 +1,19 @@
 #include "monty.h"
 
-void (*get_op_func())()
-{}
+void (*get_op_func(char *str))
+{
+	int			i = 0;
+	fae_t		farr[] = {
+		{"push", op_push},
+		{"pal", op_pal},
+		{NULL, NULL}
+	};
+
+	while (farr[i].func)
+	{
+		if (strcmp(farr[i].func, str) == 0)
+			return (farr[i].f);
+			i++;
+	}
+	return (NULL);
+}
