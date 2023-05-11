08c80eb2f442562eff995a5606151f1116adf420
Pablo <p5.tacoronte@gmail.com>
2023-05-11 00:32:26 -0700

    file with functions to manipulate a stack

---
+++ op_functions.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+* op_push- adds an elemnt to hte stack
+* @s: pointer to stack
+* @val: value to add
+* Return: pointer to the head of the stack
+*/
+stack_t *op_push(stack_t *s, int val)
+{
+	return (NULL);
+}
+
+/**
+* op_pal- prints all the elements in a stack
+* @s: pointer to stack
+* Return: void
+*/
+void op_pal(stack_t *s)
+{
+	return;
+}
