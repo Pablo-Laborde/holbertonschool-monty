cc995061a0a62c55653c67d4602d091ac865ba99
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-10 23:26:51 -0300

    add error messages

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include "monty.h"
 
+/**
+ * main - Short description
+ * @ac: first member
+ * @av: second member
+ *
+ * Return: Always 0
+ */
 int main(int ac, char **av)
 {
     if (ac != 2)
@@ -7,5 +14,19 @@ int main(int ac, char **av)
         fprintf(STDERR_FILENO, "USAGE: monty file");
         _exit(EXIT_FAILURE);
     }
-    
+
+    // open file ... or relocate code:
+    if (fp == NULL)
+    {
+        fprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]);
+        _exit(EXIT_FAILURE);
+    }
+    // code for strcmp:
+    if (!(op_codes[index].opcode))
+	{
+		fprintf(STDERR_FILENO, "L%u: unknown instruction %s\n", line_number, token[0]);
+		_exit(EXIT_FAILURE);
+	}
+
+    return (0);
 }
