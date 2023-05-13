12d6aa83fec2cd3dd768cab712933e1cf9b33ad8
Pablo <p5.tacoronte@gmail.com>
2023-05-13 00:03:09 -0700

    new function

--- main.c
+++ main.c
@@ -9,22 +9,35 @@
  */
 int main(int ac, char **av)
 {
-	FILE		*file = NULL;
-	stack_t *stack = NULL;
+	info_t	info;
 
+	init_info(&info);
 	if (ac != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
-		_exit(EXIT_FAILURE);
+		_exit_f(&info);
 	}
-
-	file = fopen(av[1], "r");
-	if (!file)
+	info.file = fopen(av[1], "r");
+	if (!info.file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
-		_exit(EXIT_FAILURE);
+		_exit_f(&info);
 	}
-	exec_cmd(&stack, file);
-	fclose(file);
+	exec_cmd(&info);
+	fclose(info.file);
 	return (0);
+}
+
+/**
+ * init_info- nulls info pointers
+ * @info: structure to null data
+ * Return: void
+ */
+void init_info(info_t *info)
+{
+	info->file = NULL;
+	info->stack = NULL;
+	info->line_number = 0;
+	info->str = NULL;
+	info->str_len = 0;
 }
