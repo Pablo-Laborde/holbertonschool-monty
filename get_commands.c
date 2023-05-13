c4343f21a00b23fe2cc27a4543651a272f55b074
Pablo <p5.tacoronte@gmail.com>
2023-05-13 00:04:21 -0700

    changes in protoypes

--- get_commands.c
+++ get_commands.c
@@ -1,46 +1,41 @@
 #include "monty.h"
 
 /**
-* exec_cmd- executes the commands in the file
-* @stack: stack
-* @file: file to read from
-* Return: void
-*/
-void exec_cmd(stack_t **stack, FILE *file)
+ * exec_cmd- executes the commands in the file
+ * @info: struct of data
+ * Return: void
+ */
+void exec_cmd(info_t *info)
 {
 	int						i;
-	unsigned int	line_number = 0;
-	char					*str = NULL,
-								*token = NULL;
-	size_t				len = 0;
+	char					*token = NULL;
 	void (*f)(stack_t **, unsigned int) = NULL;
-	stack_t *aux;
 
 	value = 0;
-	while (getline(&str, &len, file) != -1)
+	while (getline(&(info->str), &(info->str_len), info->file) != -1)
 	{
-		line_number++;
-		token = str;
+		info->line_number++;
+		token = info->str;
 		i = 0;
 		while (token[i] && token[i] <= 32)
 			token = &token[i + 1];
-		f = get_func(token, line_number);
-		free(str);
-		str = NULL;
+		f = get_func(token, info);
+		free(info->str);
+		info->str = NULL;
 		if (f)
-			f(stack, line_number);
+			f(&(info->stack), info->line_number);
 		f = NULL;
 	}
-	free(str);
+	free(info->str);
 }
 
 /**
-* get_func- looks for the correct function
-* @name: name of the function tobe loked
-* @n: as name says
-* Return: function pointer if succesfull, NULL otherwise
-*/
-void (*get_func(char *name, unsigned int n))(stack_t **, unsigned int)
+ * get_func- looks for the correct function
+ * @name: name of the function tobe loked
+ * @info: struct of data
+ * Return: function pointer if succesfull, NULL otherwise
+ */
+void (*get_func(char *name, info_t *info))(stack_t **, unsigned int)
 {
 	int			i = 0;
 	char		*val = NULL;
@@ -67,8 +62,8 @@ void (*get_func(char *name, unsigned int n))(stack_t **, unsigned int)
 					val = strtok(NULL, " \t\n$");
 					if (check_number(val))
 					{
-						fprintf(stderr, "L%u: usage: push integer\n", n);
-						_exit(EXIT_FAILURE);
+						fprintf(stderr, "L%u: usage: push integer\n", info->line_number);
+						_exit_f(info);
 					}
 					value = atoi(val);
 				}
@@ -77,16 +72,16 @@ void (*get_func(char *name, unsigned int n))(stack_t **, unsigned int)
 			i++;
 		}
 	}
-	fprintf(stderr, "L%u: unknown instruction %s\n", n, val);
-	_exit(EXIT_FAILURE);
+	fprintf(stderr, "L%u: unknown instruction %s\n", info->line_number, val);
+	_exit_f(info);
 	return (NULL);
 }
 
 /**
-* check_number- check if str is a number
-* @str: string to be checked
-* Return: 0 if true, 1 if false
-*/
+ * check_number- check if str is a number
+ * @str: string to be checked
+ * Return: 0 if true, 1 if false
+ */
 int check_number(char *str)
 {
 	int	i = 0, flag = 0;
