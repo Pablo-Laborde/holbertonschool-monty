9bdf149801a020ed86c0a4ccac7b06358abf29fe
Pablo <p5.tacoronte@gmail.com>
2023-05-15 06:21:12 -0700

    pint fix

--- op_functions_a.c
+++ op_functions_a.c
@@ -57,7 +57,7 @@ void op_pint(stack_t **stack, unsigned int line_number)
 	fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 	_exit(EXIT_FAILURE);
 	}
-	fprintf(stdout, "%d", (*stack)->n);
+	fprintf(stdout, "%d\n", (*stack)->n);
 }
 
 /**
