0b5781dcee2f916eed15a3418b6ab4aa699998ef
Pablo <p5.tacoronte@gmail.com>
2023-05-15 11:38:34 -0700

    more functions

--- get_commands.c
+++ get_commands.c
@@ -39,17 +39,11 @@ void (*get_func(char *name))(stack_t **, unsigned int)
 	int			i = 0;
 	char		*val = NULL;
 	instruction_t	arr[] = {
-		{"push", op_push},
-		{"pall", op_pall},
-		{"pint", op_pint},
-		{"pop", op_pop},
-		{"swap", op_swap},
-		{"add", op_add},
-		{"nop", op_nop},
-		{"sub", op_sub},
-		{"div", op_div},
-		{"mul", op_mul},
-		{"mod", op_mod},
+		{"push", op_push}, {"pall", op_pall}, {"pint", op_pint},
+		{"pop", op_pop}, {"swap", op_swap},	{"add", op_add},
+		{"nop", op_nop}, {"sub", op_sub}, {"div", op_div},
+		{"mul", op_mul}, {"mod", op_mod}, {"pchar", op_pchar},
+		{"pstr", op_pstr}, {"rot1", op_rot1}, {"rotr", op_rotr},
 		{NULL, NULL}
 	};
 
--- op_functions_c.c
+++ op_functions_c.c
@@ -26,3 +26,103 @@ void op_mod(stack_t **stack, unsigned int line_number)
 	(*stack)->next = NULL;
 	free(aux);
 }
+
+/**
+* op_pchar -  adds the top two elements of the stack
+* @stack: pointer to stack
+* @line_number: as name says
+* Return: void
+*/
+void op_pchar(stack_t **stack, unsigned int line_number)
+{
+	if (!stack || !(*stack))
+	{
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
+		_exit_f(1);
+	}
+	if ((*stack)->n > 127 || (*stack)->n < -128)
+	{
+		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
+		_exit_f(1);
+	}
+	fprintf(stdout, "%c\n", (*stack)->n);
+}
+
+/**
+* op_pstr -  adds the top two elements of the stack
+* @stack: pointer to stack
+* @line_number: as name says
+* Return: void
+*/
+void op_pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *aux = NULL;
+	int n;
+
+	(void)line_number;
+	aux = *stack;
+	n = aux->n;
+	while (aux && n != 0 && n <= 127 && n >= -128)
+	{
+		fprintf(stdout, "%c", n);
+		aux = aux->prev;
+		if (aux)
+			n = aux->n;
+	}
+	fprintf(stdout, "\n");
+}
+
+/**
+* op_rot1 -  adds the top two elements of the stack
+* @stack: pointer to stack
+* @line_number: as name says
+* Return: void
+*/
+void op_rot1(stack_t **stack, unsigned int line_number)
+{
+	stack_t *a0 = NULL, *a1 = NULL, *a2 = NULL;
+
+	(void)line_number;
+	if (stack)
+	{
+		a1 = *stack;
+		if (a1 && a1->prev)
+		{
+			a2 = a1->prev;
+			a0 = a2;
+			while (a2->prev)
+				a2 = a2->prev;
+			a1->next = a2;
+			a1->prev = NULL;
+			a2->prev = a1;
+			*stack = a0;
+		}
+	}
+}
+
+/**
+* op_rotr -  adds the top two elements of the stack
+* @stack: pointer to stack
+* @line_number: as name says
+* Return: void
+*/
+void op_rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *a1 = NULL, *a2 = NULL;
+
+	(void)line_number;
+	if (stack)
+	{
+		a1 = *stack;
+		if (a1 && a1->prev)
+		{
+			while (a1)
+			{
+				a2 = a1->next;
+				a1->next = a1->prev;
+				a1->prev = a2;
+				a1 = a1->next;
+			}
+		}
+	}
+}
